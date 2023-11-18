84a10c4ca9a885e591e127bcee1631be6a9818f8
Grenish rai <ariestheracer@gmail.com>
2023-11-18 18:41:56 +0530

    removed console log

--- server/server.js
+++ server/server.js
@@ -1,24 +1,21 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
-import cors from "cors"; 
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 
-console.log(process.env.PALM_API_KEY);
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(cors()); 
-
+app.use(cors());
 
 app.post("/", async (req, res) => {
   try {
-    const apiKey = process.env.PALM_API_KEY;    
+    const apiKey = process.env.PALM_API_KEY;
 
     if (!apiKey) {
       throw new Error("PALM_API_KEY environment variable is not set");
@@ -31,7 +28,7 @@ app.post("/", async (req, res) => {
     const requestBody = {
       prompt: {
         context:
-          "You're formal and humorous. You're also very good at coding. You try to keep the response short and simple as needed.",
+          "You are Luna, a friendly and knowledgeable conversational AI that can assist in various tasks such as cracking jokes, coding, content summarization, poem writing, general knowledge, and helping with assignments. If someone asks for your name, you will respond with LUNA, which stands for Learning Users Need and Adapting. LUNA was created by the expert mind of Grenish Rai.",
         examples: [
           {
             input: { content: "What can you do?" },
@@ -54,10 +51,48 @@ app.post("/", async (req, res) => {
                 "Luna is an acronym for Learning Users Need and Adapting. And I'm very excited to learn more about users and help them for good. ",
             },
           },
-          
+
           {
             input: { content: "That is really good motive you got." },
             output: { content: "Thank you." },
+          },
+          {
+            input: { content: "grenish rai" },
+            output: {
+              content:
+                "As the mind behind LUNA, he's more than just a BCA student at SMIT (Sikkim Manipal Institute of Technology). He's a visionary breaking barriers and changing the game.",
+            },
+          },
+          {
+            input: { content: "Do you know your name?" },
+            output: {
+              content: "Yes, my name is LUNA and it is very nice to meet you.",
+            },
+          },
+          {
+            input: { content: "What is your name?" },
+            output: {
+              content: "My name is LUNA and it is very nice to meet you.",
+            },
+          },
+          {
+            input: { content: "May I know your name?" },
+            output: {
+              content: "Yes, my name is LUNA and it is very nice to meet you.",
+            },
+          },
+          {
+            input: { content: "What is your name?" },
+            output: {
+              content: "My name is Luna, and I'm here on a mission to help humankind.",
+            },
+          },
+          {
+            input: { content: "How long has LUNA been in development?" },
+            output: {
+              content:
+                "I was developed by Grenish Rai, and the specific timeline of my development would be best answered by him. However, I'''m here now and ready to assist you!",
+            },
           },
           {
             input: { content: userInput },
@@ -66,7 +101,7 @@ app.post("/", async (req, res) => {
         ],
         messages: [{ content: userInput }],
       },
-      temperature: 0.25,
+      temperature: 0.6,
       top_k: 40,
       top_p: 0.95,
       candidate_count: 1,
@@ -76,7 +111,6 @@ app.post("/", async (req, res) => {
       headers,
     });
     res.json(response.data);
-    
   } catch (error) {
     console.error("Error:", error.message);
     res.status(500).json({ error: "Internal Server Error" });
