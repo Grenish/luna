68c2b9f356e858b5814d0afa4cbf445b597f9ff0
Grenish rai <ariestheracer@gmail.com>
2023-11-19 03:05:58 +0530

    assets added

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
-import luna from './luna.svg';
-import animatedlogo from './logo-animated.svg';
-import logo2 from './logo.gif';
-import defaultlogo from './defaultlogo.svg';
-import user from './user.svg';
-import send from './message.svg';
-import plus from './plus.svg';
-import copy from './clipboard.svg';
+import logo from "./logo.svg";
+import luna from "./luna.svg";
+import animatedlogo from "./logo-animated.svg";
+import logo2 from "./logo.gif";
+import defaultlogo from "./defaultlogo.svg";
+import user from "./user.svg";
+import send from "./message.svg";
+import plus from "./plus.svg";
+import copy from "./clipboard.svg";
+import flac1 from "./flac1.png";
+import flac2 from "./flac2.png";
+import flac3 from "./flac3.png";
+import flac4 from "./flac4.png";
+import copy2 from "./copy.svg";
+import check from "./check.svg";
 
-export { logo, luna, animatedlogo, logo2, defaultlogo, user, send, plus, copy };
+export {
+  logo,
+  luna,
+  animatedlogo,
+  logo2,
+  defaultlogo,
+  user,
+  send,
+  plus,
+  copy,
+  flac1,
+  flac2,
+  flac3,
+  flac4,
+  copy2,
+  check,
+};
